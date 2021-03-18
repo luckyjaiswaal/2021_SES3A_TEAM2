ed50c5ebd24790146b741319afdf1827c2452ca1
keysers0ze <kevinguemail@gmail.com>
2021-03-18 17:55:17 +1100

    Reworkd app setup, added tweeter stream functions, tweet streaming is working

---
+++ sentiment_analysis/backend/tweeter_functions/api_client.py
@@ -0,0 +1,9 @@
+import tweepy
+import config
+
+
+def generate_api_obj(consumer_key, consumer_secret, access_token, access_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    return api
