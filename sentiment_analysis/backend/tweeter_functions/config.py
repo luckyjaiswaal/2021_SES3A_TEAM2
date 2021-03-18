43d13db82f3ad2786ff658ca041b05a12b249719
Kevin Gu <kevinguemail@gmail.com>
2021-03-18 18:54:03 +1100

    Delete config.py

--- sentiment_analysis/backend/tweeter_functions/config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "B5FAOQT1qOOohjcbtqjM3GyV6"
-consumer_secret = "6h4xKVnWte9jZYfxhe2GwxK102xQT63YXIIQ4eZVF61WfvyaVR"
-access_token = "1372410525322149888-iWMwhYyqDzVBLE1ZrP4Jvo00mR3FOO"
-access_secret = "ZZ39PS974kTx5Xf4CbukozrODlPPRXDaT22zgesBm4rVw"
