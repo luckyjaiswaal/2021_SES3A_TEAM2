c7caed019f6f5d63395e44b63feeaafb0888ed99
Dsun <1513221760@qq.com>
2021-03-27 16:57:10 +0800

    dashbord front-end page about user profile and information

--- sentiment_analysis/frontend/src/components/App.js
+++ sentiment_analysis/frontend/src/components/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import './dashboard.css';
+import UserProfile from './userProfile/index';
+import {Layout} from 'antd'
+import PieChartView from '../components/pieChartView/index'
+import InvestAndMarket from '../components/investandmarket/index'
 
+const { Sider, Content, Footer } = Layout;
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <h1>Testing</h1>;
+    return(
+      <div>
+        <Layout style={{ height: 700 }}>
+          <Sider width={300} style={{backgroundColor:'#eee'}}>
+            <Content style={{ height: 200 }}>
+                <UserProfile name={'tom'}/>
+            </Content>
+            <Content style={{ height: 200 }}>
+                <InvestAndMarket marketreturn={11111} invest={1111}/>          
+            </Content>
+            <Content style={{ height: 300 }}>
+                <div className="status" >Market Status:CLOSED</div>         
+            </Content>
+          </Sider>
+          {/* <Layout>
+            <Content style={{ height: 300 }}>
+              <UserProfile name={'tom'}/>
+            </Content>
+          </Layout> */}
+        </Layout>
+      
+      </div>
+    )
   }
 }
 
---
+++ sentiment_analysis/frontend/src/components/pieChartView/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './view2.css';
+import PieChart from '../charts/pieChart/index';
+
+export default class PieChartView extends Component {
+    render() {
+        return (
+            <div id='view2' className='pane'>
+                <div className='header'>Gender</div>
+                <PieChart />
+            </div>
+        )
+    }
+}
