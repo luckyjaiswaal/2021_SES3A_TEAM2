7c95c03c0a6317034c3672a1f7d1a9ed05a5d776
keysers0ze <kevinguemail@gmail.com>
2021-03-29 00:54:27 +1100

    modified twitter stream functions

---
+++ sentiment_analysis/backend/streams/config.py
@@ -0,0 +1,4 @@
+consumer_key = "jubO7FgIj38Nw5RQQviYSWSof"
+consumer_secret = "GqFOkEqVxf7ZmmEKxXLRjf7bLHydf00izgIcOt6fcrxcoXIoEx"
+access_token = "1372410525322149888-VRhXMAbm40sgxyBd5VuK4FYa3AGGe1"
+access_secret = "8vceJG8F2XEayASvL31D60aF9TEailBfKgMUngc9nt8pA"
