3e449bf82c6903231c6eb0e213e8834a1a0a491f
Niguious <13286978@student.uts.edu.au>
2021-03-29 17:33:36 +1100

    MonkeyLearn

--- sentiment_analysis/backend/sentiment_calculator/sentiment_calculator_module.py
+++ sentiment_analysis/backend/sentiment_calculator/sentiment_calculator_module.py
@@ -1,45 +1,8 @@
-from textblob import TextBlob
+from monkeylearn import MonkeyLearn
 
-#Sentiment Analysis
-def percentage(part,whole):
- return 100 * float(part)/float(whole)
-keyword = input(“Please enter keyword or hashtag to search: “)
-noOfTweet = int(input (“Please enter how many tweets to analyze: “))
-tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
-positive = 0
-negative = 0
-neutral = 0
-polarity = 0
-tweet_list = []
-neutral_list = []
-negative_list = []
-positive_list = []
-for tweet in tweets:
- 
- #print(tweet.text)
- tweet_list.append(tweet.text)
- analysis = TextBlob(tweet.text)
- score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
- neg = score[‘neg’]
- neu = score[‘neu’]
- pos = score[‘pos’]
- comp = score[‘compound’]
- polarity += analysis.sentiment.polarity
- 
- if neg > pos:
- negative_list.append(tweet.text)
- negative += 1
-elif pos > neg:
- positive_list.append(tweet.text)
- positive += 1
- 
- elif pos == neg:
- neutral_list.append(tweet.text)
- neutral += 1
-positive = percentage(positive, noOfTweet)
-negative = percentage(negative, noOfTweet)
-neutral = percentage(neutral, noOfTweet)
-polarity = percentage(polarity, noOfTweet)
-positive = format(positive, ‘.1f’)
-negative = format(negative, ‘.1f’)
-neutral = format(neutral, ‘.1f’)
+ml = MonkeyLearn('8f323ce5580a3a90f5ab41058571de9c1f218e5e')
+data = ['I love everything about @Zendesk!', 'There's a bug in the new integration]
+model_id = 'cl_pi3C7JiL'
+result = ml.classifiers.classify(model_id, data)
+
+print(result.body)
