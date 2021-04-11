c36646a32f54c7214d3f18850b9878e82f9427bd
KevinGu <yiming.gu@accenture.com>
2021-04-12 00:11:42 +1000

    started working on dockerising app, stream changes

--- sentiment_analysis/backend/streams/config.py
+++ sentiment_analysis/backend/streams/config.py
@@ -1,4 +1,4 @@
-consumer_key = "jubO7FgIj38Nw5RQQviYSWSof"
-consumer_secret = "GqFOkEqVxf7ZmmEKxXLRjf7bLHydf00izgIcOt6fcrxcoXIoEx"
-access_token = "1372410525322149888-VRhXMAbm40sgxyBd5VuK4FYa3AGGe1"
-access_secret = "8vceJG8F2XEayASvL31D60aF9TEailBfKgMUngc9nt8pA"
+consumer_key = "6T32rd6Ina8E4qukw2r3CxV7B"
+consumer_secret = "GPOg5cge5XrstIGuaM2KqC5sj3WaRJU0IvmR8um3eV8Yv4ODZ5"
+access_token = "1372410525322149888-drQH0pHXmySAkE6D1QbpxAeQ4MCgL2"
+access_secret = "7adwYXUJZRav8TtkRLOnCWN6gy1cggd0b0Z0gSrwfZ9Ff"
--- sentiment_analysis/backend/streams/twitter_stream.py
+++ sentiment_analysis/backend/streams/twitter_stream.py
@@ -1,4 +1,5 @@
-import tweepy
+from tweepy import StreamListener
+from tweepy import Stream
 import twitter_api_client
 import config
 from datetime import timezone
@@ -12,41 +13,45 @@ access_token = config.access_token
 access_secret = config.access_secret
 
 
-class StreamListener(tweepy.StreamListener):
+class MyStreamListener(StreamListener):
     def __init__(self, kafka_producer, target_topic, stream_type):
+        super().__init__()
         self.kafka_producer = kafka_producer
         self.target_topic = target_topic
         self.stream_type = stream_type
 
     def on_status(self, status):
         if self.stream_type == 'company':
-            data = {"id": int(status.id),
-                    # "created_at": long(status.created_at.replace(tzinfo=timezone.utc).timestamp()),
-                    "text": status.text or '',
-                    "user_screen_name": str(status.user.screen_name) or '',
-                    "followers_count": int(status.user.followers_count),
-                    "retweet_count": int(status.retweet_count),
-                    "favorite_count": int(status.favorite_count)}
-
-            self.kafka_producer.send(self.target_topic, value=data)
-            print(f'produced tweet to {self.target_topic}: ', data)
-            # self.kafka_producer.flush()
+            try:
+                data = {"id": int(status.id),
+                        # "created_at": long(status.created_at.replace(tzinfo=timezone.utc).timestamp()),
+                        "text": status.text or '',
+                        "user_screen_name": str(status.user.screen_name) or '',
+                        "followers_count": int(status.user.followers_count),
+                        "retweet_count": int(status.retweet_count),
+                        "favorite_count": int(status.favorite_count)}
+                # print(f"Payload to be sent {data}")
+                self.kafka_producer.send(self.target_topic, value=data)
+                print(f'produced tweet to {self.target_topic}: ', data)
+                # self.kafka_producer.flush()
+            except AssertionError as e:
+                print(f"{e} Error processing {data}")
 
 
 def create_stream(api, stream_type, kafka_producer, target_topic, tracks):
-    myStreamListener = StreamListener(
-        kafka_producer, target_topic, stream_type)
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(track=['tesla'])
+    myStreamListener = MyStreamListener(kafka_producer, target_topic, stream_type)
+    myStream = Stream(auth=api.auth, listener=myStreamListener)
+    myStream.filter(track=tracks)
 
 
 def main():
-    api = twitter_api_client.generate_api_obj(
-        consumer_key, consumer_secret, access_token, access_secret)
-    kafka_producer = KafkaProducer(bootstrap_servers='localhost:2181')
+    print("Starting stream")
+    api = twitter_api_client.generate_api_obj(consumer_key, consumer_secret, access_token, access_secret)
+    kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    print(f"Connected to Twitter api :{api}")
     target_topic = "twitter_raw"
     stream_type = 'company'
-    create_stream(api, stream_type, kafka_producer, target_topic, [])
+    create_stream(api, stream_type, kafka_producer, target_topic, ['tsla', 'tesla'])
 
 
 if __name__ == "__main__":
