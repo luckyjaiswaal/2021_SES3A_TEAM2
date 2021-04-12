427c164325f2a31aa3a8495594bb5be4911b062a
Ian36 <ianhuang7991@gmail.com>
2021-04-12 13:20:06 +1000

    separated react and django app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard></Dashboard>
+    </div>
+  );
+}
+
+export default App;
--- sentiment_analysis/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Company
-
-
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = ('id', 'name', 'ticker', 'latest_sentiment_score',
-                  'latest_stock_price', 'daily_sentiment_trend', 'daily_stock_trend')
