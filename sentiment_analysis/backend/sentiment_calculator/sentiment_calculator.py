f6b68197ccf167e792a6825a1f24ea2a3c2c48bf
Niguious <13286978@student.uts.edu.au>
2021-04-15 23:16:11 +1000

    Update 15/4/2021

--- sentiment_analysis/backend/sentiment_calculator/sentiment_calculator.py
+++ sentiment_analysis/backend/sentiment_calculator/sentiment_calculator.py
@@ -2,9 +2,7 @@ from monkeylearn import MonkeyLearn
 
 
 class sentiment_calculator:
-    message = "Pull Twitter message in here"
-
-    def sentiment_score:
+    def sentiment_score(message):
         ml = MonkeyLearn('8f323ce5580a3a90f5ab41058571de9c1f218e5e')
         model_id = 'cl_pi3C7JiL'
         result = ml.classifiers.classify(model_id, message)
