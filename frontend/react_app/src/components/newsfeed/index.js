a93c2ca5e036dc0cc9af23f1fe23e2287b989a0d
harrisontruong1 <h.vstruong@gmail.com>
2021-04-19 15:29:14 +1000

    Newsfeed update

--- frontend/react_app/src/components/newsfeed/index.js
+++ frontend/react_app/src/components/newsfeed/index.js
@@ -1,36 +1,49 @@
 import './newsfeed.css';
 import React, { Component } from "react";
-import { Table, Container } from "@material-ui/core";
+import { Table, TableRow, TableCell, Container } from "@material-ui/core";
 
+
+function createData(story, date) {
+    return {story,date};
+  }
+  
+
+const rows = [
+    createData('Story1', '19/04'),
+    createData('Story2', '19/04'),
+    createData('Story3', '19/04'),
+    createData('Story4', '19/04'),
+  ];
 
 export default class Newsfeed extends Component {
     render() {
         return (
-<div>
-    <Table striped boredered hover variant="dark">
-        <tr>
-            <th>NewsFeed</th>
-        </tr>
-        <tr>
 
+<div>
+    <Table>
+        <TableRow style={{backgroundColor:'#363538'}}>
+            <TableCell style={{color: '#F6F6F6'}}>NewsFeed</TableCell>
+        </TableRow>
+        
     <Container className="newsfeed">
-        <p>
-            Testing
-        </p>
-        <p>
-            Testing
-        </p>
-        <p>
-            Testing
-        </p>
-        <p>
-            Testing
-        </p>
-        <p>
-            Testing
-        </p>
+    {rows.map((row) => (
+                  <TableRow key={row.stock}>
+                    <TableCell component="th" scope="row">
+                      {row.stock}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.symbol}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.price}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.score}
+                    </TableCell>
+                    </TableRow>
+                    
     </Container>
-        </tr>
+        
     </Table>
 </div>
 
