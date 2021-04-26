7737b8af75d8fea35ac1e3acb946887e0256ee4a
KevinGu <yiming.gu@accenture.com>
2021-04-26 12:43:30 +1000

    add kafka consumer, test vader and textblob

--- CORE/streams/twitter/twitter_consumer.py
+++ CORE/streams/twitter/twitter_consumer.py
@@ -1,9 +1,13 @@
+
+
 from kafka import KafkaConsumer
 from datetime import timezone
 import json
 import sys
-sys.path.insert(1, '/Users/yiming.gu/Desktop/Personal/U/3A_MASTER/2021_SES3A_TEAM2/CORE')
-from nlp import tweets_processor
+sys.path.insert(
+    1, '/Users/yiming.gu/Desktop/Personal/U/3A fml/2021_SES3A_TEAM2/CORE')
+from nlp import tweets_processor, nlp_twitter
+
 kafka_topic = 'twitterdata'
 
 
@@ -31,8 +35,12 @@ def main():
 
     for message in kafka_consumer:
         if message.value is not None:
-            tweets = json.loads(json.dumps(message.value))
-            processed_tweet = tweets_processor.preprocess_tweet(tweets)
+            tweet = json.loads(json.dumps(message.value))
+            processed_tweet = tweets_processor.preprocess_tweet(tweet)
+
+            sent_score_vader = nlp_twitter.calculate_sentiment_vader(processed_tweet)
+            sent_score_txtblob = nlp_twitter.calculate_sentiment_textblob(processed_tweet)
+            print(f"Raw message: {message.value['text']} \nPreprocessed: {processed_tweet} \nVader:{sent_score_vader} \nTextblob:{sent_score_txtblob} \n\n\n")
 
 
 if __name__ == "__main__":
