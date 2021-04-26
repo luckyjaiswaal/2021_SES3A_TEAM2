b8c31bee2ea64baa9054c6fa9091fca7cf15c781
Ian36 <ianhuang7991@gmail.com>
2021-04-26 14:38:39 +1000

    Added charts  to the indepth page

--- frontend/react_app/src/InDepth.js
+++ frontend/react_app/src/InDepth.js
@@ -4,6 +4,8 @@ import NavBar from './components/navbar/index';
 import StockList from './components/stocklist/index';
 import axios from './components/utils/axios'
 import './indepth.css'
+import React from 'react';
+import { Line } from 'react-chartjs-2';
 
 function Indepth({ match }) {
   useEffect(() => {
@@ -21,33 +23,60 @@ function Indepth({ match }) {
     console.log(item);
   }
 
+  const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: 'Sample data, fill with real data from API',
+        data: [12, 19, 3, 5, 2, 3],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <>
-    <div className="indepth">
-            <div className="top">
-                <div className="searchArea">
-                    <input type="search" placeholder="Search For Stock" />
-                </div>
-                <div className="dashboard-btn">
-                    <button className="btn">Dashboard</button>
-                </div>
-            </div>
-            <div className="section">
-                <div className="time">
-
-<h3>Search Bar stock to be displayed in the bar.</h3> {fetchItem}
-                </div>
-                <div className="chart">
-                    <div className="sentiment">
-                        <h3>Sentiment Chart</h3>
-                    </div>
-                    <div className="pie">
-<h3>Pie Chart</h3>
-                    </div>
-                </div>
-
-            </div>
+      <div className="indepth">
+        <div className="top">
+          <div className="searchArea">
+            <input type="search" placeholder="Search For Stock" />
+          </div>
+          <div className="dashboard-btn">
+            <button className="btn">Dashboard</button>
+          </div>
         </div>
+        <div className="section">
+          <div className="time">
+
+            <h3>Search Bar stock to be displayed in the bar.</h3> {fetchItem}
+          </div>
+          <div className="chart">
+            <div className="sentiment">
+              <h3>Sentiment Score Chart</h3>
+              <Line data={data} options={options} />
+            </div>
+            <div className="pie">
+              <h3>Share Price Chart</h3>
+              <Line data={data} options={options} />
+            </div>
+          </div>
+
+        </div>
+      </div>
       {/* <div className="header">
         <h1> In-depth information for {stockSymbol}</h1>
         <h2>Stock info is currently blocked due to CORS policy, possible fix is to fetch from backend instead.</h2>
