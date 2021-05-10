f54c5311f465b77948b04004740101fa175f0095
Matthew Veldhuizen <37282829+matthew-veldhuizen@users.noreply.github.com>
2021-05-10 13:33:58 +1000

    Made elements universally available, removed old code, configured menu buttons
    Made card, stocklist.json and searchbar elements all more easily accessible
    removed old code
    Configured UI elements

--- frontend/react_app/src/SearchBar.js
+++ frontend/react_app/src/SearchBar.js
@@ -1,10 +1,11 @@
-import './stocklist.css';
+import './components/stocklist/stocklist.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 const SearchBar = ({onChange, placeholder}) => {
     return (
     <div className="Search">
       <span className="SearchSpan">
-        
+        <SearchIcon />
       </span>
       <input
         className="SearchInput"
